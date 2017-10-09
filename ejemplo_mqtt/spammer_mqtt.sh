3cf480dac6dc0d53ea95e75f8b7ad0300af08841
LucianoFromTrelew <lucianoserruya@hotmail.com>
2017-10-09 14:29:56 -0300

    agregando variables al script

--- ejemplo_mqtt/spammer_mqtt.sh
+++ ejemplo_mqtt/spammer_mqtt.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-for i in $(seq 1 100); do 
-    mosquitto_pub -h localhost -t out -m 100 
-    sleep 5
+TOPIC='in'
+MAX=1000
+TIME=2
+
+for i in $(seq 1 $MAX); do 
+    mosquitto_pub -h localhost -t "$TOPIC" -m $i 
+    sleep $TIME
 done
